aad078e4c8d731812eac6b64b185ec199ab8810f
Daniel Bojdo <daniel@bojdo.eu>
2015-03-15 07:29:03 +0000

    [upd] mapping exception

---
+++ src/Exception/MappingException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * File MappingException.php
+ * Created at: 2015-03-15 07-28
+ *
+ * @author Daniel Bojdo <daniel.bojdo@web-it.eu>
+ */
+
+namespace Webit\Shipment\GlsAdapter\Exception;
+
+class MappingException extends \RuntimeException
+{
+
+}
